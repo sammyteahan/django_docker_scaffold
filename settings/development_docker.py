990f75662c6acf6232f3fa21b63944b339d9d96a
sammyteahan <sammyteahan@gmail.com>
2017-04-18 13:34:52 -0600

    Adding redis as a dependency and improving celery setup with an example task

--- settings/development_docker.py
+++ settings/development_docker.py
@@ -3,6 +3,10 @@ from settings.common import *
 DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.11/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', ]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
