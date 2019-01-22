c640595e763fb622bbc8374eab781a810e8089f1
sammyteahan <sammyteahan@gmail.com>
2019-01-22 08:53:04 -0700

    Django 2
    Update dependencies for Django 2.1

--- apps/core/tasks.py
+++ apps/core/tasks.py
@@ -1,7 +1,6 @@
 from celery import Celery
 
 from django.conf import settings
-from django.core.urlresolvers import reverse_lazy
 
 app = Celery('apps.core.tasks', broker=settings.BROKER_URL)
 app.conf.update(settings.CELERY_SETTINGS)
